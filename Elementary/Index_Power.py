9cde28ee3519476f830eca690df1e805f2491354
nazna <14846330+nazna@users.noreply.github.com>
2017-04-23 01:39:16 +0900

    add: リストの添え字の要素を取り出して添え字だけ累乗する関数の実装

---
+++ Elementary/Index_Power.py
@@ -0,0 +1,8 @@
+def index_power(array, n):
+    return array[n]**n if n < len(array) else -1
+
+if __name__ == '__main__':
+    assert index_power([1, 2, 3, 4], 2) == 9, "Square"
+    assert index_power([1, 3, 10, 100], 3) == 1000000, "Cube"
+    assert index_power([0, 1], 0) == 1, "Zero power"
+    assert index_power([1, 2], 3) == -1, "IndexError"
