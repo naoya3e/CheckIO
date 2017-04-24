cec4c494d4b04ba1bf2c4fc87f76210e7f033ef8
nazna <14846330+nazna@users.noreply.github.com>
2017-04-24 18:32:30 +0900

    add: 0以外の数値すべての積を計算する関数の実装

---
+++ Elementary/Digits_Multiplication.py
@@ -0,0 +1,10 @@
+from functools import reduce
+
+def checkio(number):
+    return reduce(lambda a, b: a * b, [int(x) for x in str(number) if x != '0'])
+
+if __name__ == '__main__':
+    assert checkio(123405) == 120
+    assert checkio(999) == 729
+    assert checkio(1000) == 1
+    assert checkio(1111) == 1
